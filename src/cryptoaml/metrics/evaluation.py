d35bf07a139904438c03ba8f689fe8e752ff405f
achmand <dylan.vassallo@hotmail.com>
2020-02-22 11:20:46 +0100

    Add confusion matrix metric

--- src/cryptoaml/metrics/evaluation.py
+++ src/cryptoaml/metrics/evaluation.py
@@ -9,12 +9,14 @@ The following models are included;
 from sklearn.metrics import f1_score
 from sklearn.metrics import recall_score
 from sklearn.metrics import precision_score
+from sklearn.metrics import confusion_matrix
 
 ###### Evaluation functions ###############################################
 F1_BINARY        = "f1"
 F1_MICRO         = "f1_micro"
 RECALL_BINARY    = "recall"
 PRECISION_BINARY = "precision"
+CONFUSION_MATRIX = "confusion"
 
 def compute_f1_binary(y_true, y_pred):
     return f1_score(y_true, y_pred, average="binary")
@@ -28,12 +30,16 @@ def compute_recall_binary(y_true, y_pred):
 def compute_precision_binary(y_true, y_pred):
     return precision_score(y_true, y_pred, average="binary")
 
+def compute_confusion_matrix(y_true, y_pred):
+    return confusion_matrix(y_true, y_pred)
+
 # Map different metric constants to metric function
 eval_options = {
     F1_BINARY:        compute_f1_binary,
     F1_MICRO:         compute_f1_micro,
     RECALL_BINARY:    compute_recall_binary,
-    PRECISION_BINARY: compute_precision_binary
+    PRECISION_BINARY: compute_precision_binary, 
+    CONFUSION_MATRIX: compute_confusion_matrix
 }
 
 def evaluate(metrics, y_true, y_pred):
