1388c11cd2fbf38886ea049be1c97c028575f26a
achmand <dylan.vassallo@hotmail.com>
2020-03-02 22:25:45 +0100

    Refactored code and re-added model persistence

--- src/cryptoaml/models/__init__.py
+++ src/cryptoaml/models/__init__.py
@@ -3,10 +3,10 @@ Top level API for models used in experiments.
 """
 
 # Author: Dylan Vassallo <dylan.vassallo.18@um.edu.mt>
-from ._models import XgboostAlgo
+from ._models import XgboostAlgo, CatBoostAlgo
 
 ###### exposed functionality ##############################################
-__all__ = ["XgboostAlgo"]
+__all__ = ["XgboostAlgo", "CatBoostAlgo"]
 
 # from .models import (RandomForestAlgo,
 #                      AdaBoostAlgo,
--- src/cryptoaml/models/_models.py
+++ src/cryptoaml/models/_models.py
@@ -1,11 +1,11 @@
 """
-Boosting classifiers tested in the experiments.
-The following models are included; 
+
 """
 
 # Author: Dylan Vassallo <dylan.vassallo.18@um.edu.mt>
 
 ###### Importing dependencies #############################################
+import json
 import pickle
 import numpy as np 
 import pandas as pd
@@ -24,7 +24,7 @@ from catboost import CatBoostClassifier
 from sklearn.ensemble import RandomForestClassifier
 
 # TODO -> Model fitted check & Validation where neeeded 
-# TODO -> Persistence layer 
+# TODO -> Implement logger 
 
 ###### Constants #########################################################
 MODEL_BASE  = "model_base"
@@ -32,6 +32,10 @@ MODEL_RF    = "random_forest"
 MODEL_XGB   = "xg_boost"
 MODEL_LIGHT = "light_boost"
 MODEL_CAT   = "cat_boost"
+
+PERSIST_SAVE      = "save"
+PERSIST_LOAD      = "load"
+PERSIST_LOAD_SAVE = "load_save" # TODO -> Not yet implemented 
 
 ###### Base classifier ###################################################
 class _BaseAlgo(ABC, BaseEstimator, ClassifierMixin):
@@ -45,12 +49,23 @@ class _BaseAlgo(ABC, BaseEstimator, ClassifierMixin):
     @abstractmethod
     def __init__(self, 
                  tune_props=None, 
+                 persist_props=None,
                  **kwargs):
         self._model = None 
         self._model_name = MODEL_BASE
         self._tuner = None 
         self._tune_props = tune_props
+        self._init_model(**kwargs)
 
+        # Convert persistence properties to Namespace
+        self._persist_props = persist_props
+        if persist_props != None:
+            self._persist_props = u.Namespace(persist_props)
+
+            # Load model
+            if self._persist_props.method == PERSIST_LOAD:
+                self._load(self._persist_props.load_path)                       
+            
     # Properties ----------------------------------------------------------
     @property
     def model_name_(self):
@@ -71,43 +86,89 @@ class _BaseAlgo(ABC, BaseEstimator, ClassifierMixin):
         check_is_fitted(self._model, ["feature_importances_"])
         return (self._tuner.meta_results_, self._tuner.results_)
 
-    # Parameters functions ------------------------------------------------
+    # Init/Parameters functions -------------------------------------------
+    @abstractmethod 
+    def _init_model(self, **kwargs):
+        pass
+
     def set_params(self, **params):
         return self._model.set_params(**params)
     
     def get_params(self, deep=True):
         return self._model.get_params(deep)
-    
+
     # Train/Tune/Evaluate functions ---------------------------------------
     def fit(self, X, y):
         
-        # Keep a reference of features utilised 
-        self._features = X.columns 
+        # Keep a reference of features 
+        self._features = X.columns.values
 
         # Fit model 
-        if self._tune_props == None: # Without hyperparameter tuning 
+        # No hyperparameter tuning 
+        if self._tune_props == None: 
             self._model.fit(X, y)
-        else:                        # Hyperparameter tuning 
+        # Hyperparameter tuning 
+        else:                        
             self._tuner = tu.tune_model(self._model, X, y, self._tune_props)
             self._model = self._tuner.best_estimator_
-    
+        
+        # Persist model after training 
+        if self._persist_props != None and self._persist_props.method == PERSIST_SAVE:
+            self._save(self._persist_props.save_path)
+
     def predict(self, X):
         return self._model.predict(X)
   
     def evaluate(self, metrics, X, y):
         y_pred = self.predict(X)
         return ev.evaluate(metrics, y, y_pred)
-    
+
+    # Persistence functions -----------------------------------------------
+    def _save(self, path):
+        
+        # Make sure model is fitted before saving 
+        check_is_fitted(self._model, ["feature_importances_"])
+
+        # Save model 
+        source_path = path + "/" + self._model_name
+        u.create_dir(source_path)
+        with open(source_path + "/" + self._model_name + ".pkl", "wb") as model_file:
+            pickle.dump(self._model, model_file)
+
+        # Save meta data
+        meta_data = {}
+        meta_data["features"] = self._features
+        with open(source_path + "/" + self._model_name + "_meta.pkl", "wb") as meta_file:
+            pickle.dump(meta_data, meta_file, pickle.HIGHEST_PROTOCOL)
+
+    def _load(self, path):
+        # Load model
+        with open(path + "/" +  path.split("/")[-1] + ".pkl", "rb") as model_file:
+            tmp_model = pickle.load(model_file)
+            if type(tmp_model) != type(self._model):
+                raise TypeError("model loaded is of type '{}' but expecting '{}'".format(type(tmp_model), type(self._model)))
+            self._model = tmp_model
+
+        # Load meta data 
+        with open(path + "/" +  path.split("/")[-1] + "_meta.pkl", "rb") as meta_file:
+            meta_data = pickle.load(meta_file)
+            self._features = meta_data["features"]
+
 ###### Random Forest classifier ##########################################
 class RandomForestAlgo(_BaseAlgo): 
 
     # Constructor ---------------------------------------------------------
     def __init__ (self, 
                  tune_props=None, 
+                 persist_props=None,
                  **kwargs): 
         super ().__init__(
-            tune_props = tune_props
+            tune_props = tune_props, 
+            persist_props = persist_props
         )
+        
+    # Init/Parameters functions -------------------------------------------
+    def _init_model(self, **kwargs):
         self._model_name = MODEL_RF
         self._model = RandomForestClassifier(**kwargs)
 
@@ -117,10 +178,15 @@ class XgboostAlgo(_BaseAlgo):
     # Constructor ---------------------------------------------------------
     def __init__ (self, 
                  tune_props=None, 
+                 persist_props=None,
                  **kwargs): 
         super ().__init__(
-            tune_props = tune_props
+            tune_props = tune_props,
+            persist_props = persist_props
         )
+
+    # Init/Parameters functions -------------------------------------------
+    def _init_model(self, **kwargs):
         self._model_name = MODEL_XGB
         self._model = xgb.XGBClassifier(**kwargs)
 
@@ -130,10 +196,15 @@ class LightGbmAlgo(_BaseAlgo):
     # Constructor ---------------------------------------------------------
     def __init__ (self, 
                  tune_props=None, 
+                 persist_props=None,
                  **kwargs): 
         super ().__init__(
-            tune_props = tune_props
+            tune_props = tune_props,
+            persist_props = persist_props
         )
+
+    # Init/Parameters functions -------------------------------------------
+    def _init_model(self, **kwargs):   
         self._model_name = MODEL_LIGHT
         self._model = lgb.LGBMClassifier(**kwargs)
     
@@ -143,10 +214,43 @@ class CatBoostAlgo(_BaseAlgo):
     # Constructor ---------------------------------------------------------
     def __init__ (self, 
                  tune_props=None, 
+                 persist_props=None,
                  **kwargs): 
         super ().__init__(
-            tune_props = tune_props
+            tune_props = tune_props,
+            persist_props = persist_props
         )
+    
+    # Init/Parameters functions -------------------------------------------
+    def _init_model(self, **kwargs):
         self._model_name = MODEL_CAT
         self._model = CatBoostClassifier(**kwargs)
-    
+
+# ###### Models functions ##################################################
+# def get_models(models):
+#     models_collection = OrderedDict()
+#     for model in models:
+#         tmp_model = None 
+#         tmp_model_name = model
+#         tmp_model_kwagrs = {}
+
+#         pass_params = type(model) == tuple 
+#         if pass_params:
+#             tmp_model_name = model[0]
+#             tmp_model_kwagrs = model[1]
+
+#         if tmp_model_name == MODEL_RF:
+#             tmp_model = RandomForestAlgo(**tmp_model_kwagrs) 
+#         elif tmp_model_name == MODEL_XGB:
+#             tmp_model = XgboostAlgo(**tmp_model_kwagrs)
+#         elif tmp_model_name == MODEL_LIGHT:
+#             tmp_model = LightGbmAlgo(**tmp_model_kwagrs) 
+#         elif tmp_model_name == MODEL_CAT:
+#             tmp_model = CatBoostAlgo(**tmp_model_kwagrs) 
+#         else:
+#             error = "'model'=%r is not implemented" % tmp_model_name
+#             raise NotImplementedError(error)
+            
+#         models_collection[tmp_model_name] = tmp_model
+
+#     return models_collection
