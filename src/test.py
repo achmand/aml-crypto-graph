65196361a01c9adb420a4bc0221abfbfe111cc22
achmand <dylan.vassallo@hotmail.com>
2020-04-08 01:37:58 +0200

    Minor chhange

--- src/test.py
+++ src/test.py
@@ -1,185 +1,88 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-
 import numpy as np
-import time
-import sys
-
-import numpy as np
-import time
-import sys
-import pandas as pd
-import os
+import sklearn.datasets
+import sklearn.metrics
+import xgboost as xgb
+import optuna
+# save study
+import pickle
 
 
-# class to define a Graph Convolutional Layer
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import StratifiedKFold
+import cryptoaml.datareader as cdr
+from cryptoaml.models import XgboostAlgo
 
-class GraphConv(nn.Module):
-    def __init__(self, in_features, out_features, activation  = 'relu', skip = False, skip_in_features = None):
-        super(GraphConv, self).__init__()
-        self.W = torch.nn.Parameter(torch.DoubleTensor(in_features, out_features))
-        nn.init.xavier_uniform_(self.W)
+elliptic = cdr.get_data("elliptic")
+data = elliptic.train_test_split(train_size=0.7, feat_set="LF")
+
+train_X = data.train_X
+train_y = data.train_y
+
+test_X = data.test_X
+test_y = data.test_y
+
+tmp_estimator = XgboostAlgo()
+def objective(trial):
+    
+    param = {
+        # using RS
+        "learning_rate": trial.suggest_loguniform("learning_rate", 0.01, 1.0),       
+        "n_estimators": trial.suggest_int("n_estimators", 100, 800, 25),
+        "tree_method":"gpu_hist", 
+        "predictor":"gpu_predictor"
         
-        self.set_act = False
-        if activation == 'relu':
-            self.activation = nn.ReLU()
-            self.set_act = True
-        elif activation == 'softmax':
-            self.activation = nn.Softmax(dim = 1)
-            self.set_act = True
-        else:
-            self.set_act = False
-            raise ValueError("activations supported are 'relu' and 'softmax'")
-            
-        self.skip = skip
-        if self.skip:
-            if skip_in_features == None:
-                raise ValueError("pass input feature size of the skip connection")
-            self.W_skip = torch.nn.Parameter(torch.DoubleTensor(skip_in_features, out_features)) 
-            nn.init.xavier_uniform_(self.W)
-        
-    def forward(self, A, H_in, H_skip_in = None):
-        # A must be an n x n matrix as it is an adjacency matrix
-        # H is the input of the node embeddings, shape will n x in_features
-        self.A = A
-        self.H_in = H_in
-        A_ = torch.add(self.A, torch.eye(self.A.shape[0]).double())
-        D_ = torch.diag(A_.sum(1))
-        # since D_ is a diagonal matrix, 
-        # its root will be the roots of the diagonal elements on the principle diagonal
-        # since A is an adjacency matrix, we are only dealing with positive values 
-        # all roots will be real
-        D_root_inv = torch.inverse(torch.sqrt(D_))
-        A_norm = torch.mm(torch.mm(D_root_inv, A_), D_root_inv)
-        # shape of A_norm will be n x n
-        
-        H_out = torch.mm(torch.mm(A_norm, H_in), self.W)
-        # shape of H_out will be n x out_features
-        
-        if self.skip:
-            H_skip_out = torch.mm(H_skip_in, self.W_skip)
-            H_out = torch.add(H_out, H_skip_out)
-        
-        if self.set_act:
-            H_out = self.activation(H_out)
-            
-        return H_out
-        
+#         "max_depth": trial.suggest_int("max_depth", 1, 12),
+#         "subsample": trial.suggest_uniform("subsample", 0.9, 1.0),
+#         "colsample_bytree": trial.suggest_uniform("colsample_bytree", 0.9, 1.0),       
+#         "min_child_weight": trial.suggest_int("min_child_weight", 0, 4),
+#         "max_delta_step": trial.suggest_int("max_delta_step", 0, 1),
+#         "gamma": trial.suggest_loguniform("gamma", 0.0, 0.01),       
+#         "reg_lambda": trial.suggest_loguniform("reg_lambda", 1e-8, 1.0),
+#         "reg_alpha": trial.suggest_loguniform("reg_alpha", 1e-8, 1.0)
+    }
 
-# class for 2 layer Graph Convolutional Network
+    tmp_estimator.set_params(**param)
+    scores = cross_val_score(tmp_estimator, 
+                                 train_X, 
+                                 train_y, 
+                                 scoring="neg_log_loss", 
+                                 cv=StratifiedKFold(n_splits=3),
+                                 n_jobs=1)
+    
+    print(tmp_estimator.get_params())
+    mean_score = scores.mean()
+    trial.set_user_attr("cv_mean", mean_score)    
+    std_score  = scores.std()
+    trial.set_user_attr("cv_std", std_score)
+    min_score  = scores.min()
+    trial.set_user_attr("cv_min", min_score)
+    max_score  = scores.max()
+    trial.set_user_attr("cv_max", max_score)
 
-class GCN_2layer(nn.Module):
-    def __init__(self, in_features, hidden_features, out_features, skip = False):
-        super(GCN_2layer, self).__init__()
-        self.skip = skip
-        
-        self.gcl1 = GraphConv(in_features, hidden_features)
-        
-        if self.skip:
-            self.gcl_skip = GraphConv(hidden_features, out_features, activation = 'softmax', skip = self.skip,
-                                  skip_in_features = in_features)
-        else:
-            self.gcl2 = GraphConv(hidden_features, out_features, activation = 'softmax')
-        
-    def forward(self, A, X):
-        out = self.gcl1(A, X)
-        if self.skip:
-            out = self.gcl_skip(A, out, X)
-        else:
-            out = self.gcl2(A, out)
-            
-        return out
+    return mean_score
 
-def load_data(start_ts, end_ts):
-	classes_csv = 'data/elliptic/elliptic_txs_classes.csv'
-	edgelist_csv = 'data/elliptic/elliptic_txs_edgelist.csv'
-	features_csv = 'data/elliptic/elliptic_txs_features.csv'
+# study = optuna.create_study(sampler=optuna.samplers.TPESampler(), 
+#                             direction="maximize")
 
-	classes = pd.read_csv(classes_csv, index_col = 'txId') # labels for the transactions i.e. 'unknown', '1', '2'
-	edgelist = pd.read_csv(edgelist_csv, index_col = 'txId1') # directed edges between transactions
-	features = pd.read_csv(features_csv, header = None, index_col = 0) # features of the transactions
-	
-	num_features = features.shape[1]
-	num_tx = features.shape[0]	
-	total_tx = list(classes.index)
 
-	# select only the transactions which are labelled
-	labelled_classes = classes[classes['class'] != 'unknown']
-	labelled_tx = list(labelled_classes.index)
 
-	# to calculate a list of adjacency matrices for the different timesteps
+study = optuna.create_study(sampler=optuna.samplers.RandomSampler(), 
+                            direction="maximize")
 
-	adj_mats = []
-	features_labelled_ts = []
-	classes_ts = []
-	num_ts = 49 # number of timestamps from the paper
 
-	for ts in range(start_ts, end_ts):
-	    features_ts = features[features[1] == ts+1]
-	    tx_ts = list(features_ts.index)
-	    
-	    labelled_tx_ts = [tx for tx in tx_ts if tx in set(labelled_tx)]
-	    
-	    # adjacency matrix for all the transactions
-	    # we will only fill in the transactions of this timestep which have labels and can be used for training
-	    adj_mat = pd.DataFrame(np.zeros((num_tx, num_tx)), index = total_tx, columns = total_tx)
-	    
-	    edgelist_labelled_ts = edgelist.loc[edgelist.index.intersection(labelled_tx_ts).unique()]
-	    for i in range(edgelist_labelled_ts.shape[0]):
-	        adj_mat.loc[edgelist_labelled_ts.index[i], edgelist_labelled_ts.iloc[i]['txId2']] = 1
-	    
-	    adj_mat_ts = adj_mat.loc[labelled_tx_ts, labelled_tx_ts]
-	    features_l_ts = features.loc[labelled_tx_ts]
-	    
-	    adj_mats.append(adj_mat_ts)
-	    features_labelled_ts.append(features_l_ts)
-	    classes_ts.append(classes.loc[labelled_tx_ts])
+# study = optuna.create_study(sampler=optuna.samplers.GridSampler({
+#     "n_estimators": [100, 125, 150, 175, 200, 225, 250, 275, 300, 325, 350, 375, 400]}), 
+#                             direction="maximize")
 
-	return adj_mats, features_labelled_ts, classes_ts
+study.set_user_attr("k_folds", 3)
+study.set_user_attr("cv_method", "StratifiedKFold")
+# study.optimize(objective, n_trials=10)
+study.optimize(objective, n_trials=100, n_jobs=1)
 
-	
-num_features = 166
-num_classes = 2
-num_ts = 49
-epochs = 50
-lr = 0.001
-max_train_ts = 34
-train_ts = np.arange(max_train_ts)
+# display(optuna.visualization.plot_optimization_history(study))
+print(study.best_trial)
+display(study.trials_dataframe())
 
-adj_mats, features_labelled_ts, classes_ts = load_data( 0, max_train_ts)
 
-# 0 - illicit, 1 - licit
-labels_ts = []
-for c in classes_ts:
-    labels_ts.append(np.array(c['class'] == '2', dtype = np.long))
-
-gcn = GCN_2layer(num_features, 32, num_classes)
-train_loss = nn.CrossEntropyLoss(weight = torch.DoubleTensor([0.7, 0.3]))
-optimizer = torch.optim.Adam(gcn.parameters(), lr = lr)
-
-# Training
-
-for ts in train_ts:
-    A = torch.tensor(adj_mats[ts].values)
-    X = torch.tensor(features_labelled_ts[ts].values)
-    L = torch.tensor(labels_ts[ts], dtype = torch.long)
-    for ep in range(epochs):
-        t_start = time.time()
-        
-        gcn.train()
-        optimizer.zero_grad()
-        out = gcn(A, X)
-
-        loss = train_loss(out, L)
-        train_pred = out.max(1)[1].type_as(L)
-        acc = (train_pred.eq(L).double().sum())/L.shape[0]
-
-        loss.backward()
-        optimizer.step()
-
-        sys.stdout.write("\r Epoch %d/%d Timestamp %d/%d training loss: %f training accuracy: %f Time: %s"
-                         %(ep, epochs, ts, max_train_ts, loss, acc, time.time() - t_start)
-                        )
-
-# torch.save(gcn.state_dict(), str(os.path.join(MODEL_DIR, "gcn_weights.pth")))
+with open("gs_xgboost_LF.pkl", "wb") as model_file:
+    pickle.dump(study, model_file)
